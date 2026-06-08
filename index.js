811dd6c3fea51a22bd2790049ccb7d1c5c586ce3
Gabriel Teles <bilebile2020@gmail.com>
2026-06-08 17:04:15 -0300

    Cria galeria de imagens

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const menuBtn = document.getElementById("toggle-mobile");
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("w3-hide")
 });
+
+// Carousel toggle
+const carousel = document.querySelectorAll(".carousel-img");
+const previews = document.querySelectorAll(".preview-img");
+
+function displayCarouselImg(number) {
+    carousel.forEach(elem => elem.classList.add("w3-hide"));
+    document.getElementById(`carousel-${number}`).classList.remove("w3-hide");
+    previews.forEach(elem => elem.classList.add("w3-grayscale-max"));
+    document.getElementById(`preview-${number}`).classList.remove("w3-grayscale-max");
+}
